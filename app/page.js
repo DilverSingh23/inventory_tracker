85b02026a7349cdfe8e4898bfbed2c4afc5388d6
DilverSingh23 <dsinghh23@gmail.com>
2024-08-06 01:59:11 -0400

    Edit add new item

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgColor: 'white',
+  bgcolor: 'white',
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
